20c06e8c9b6cc65606d8aecace1164d29c1caf30
Flip12345 <filip.kadlec.s@gyarab.cz>
2026-01-23 06:26:47 +0100

    Update exchange.py to fetch data from a new URL

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,4 +1,7 @@
 import httpx
-r = httpx.get("https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt?date=15.01.2026")
-lines = r.text.split("\n")
+
+r = httpx.get('URL z ukolu 2')
+print(r.text)
+print(r.status)
+lines = r.text.split('\n')
 print(lines[0])
