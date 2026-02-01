0974056f191cc276cc46a0b7dd176cd69f81dd10
Flip12345 <filip.kadlec.s@gyarab.cz>
2026-02-01 18:32:58 +0100

    Update exchange.py

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -2,7 +2,7 @@ import httpx
 
 res = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt')
 
-print("Server odpovedel:", res.status_code)
+print("server odpovedel:", res.status_code)
 lines = res.text.split('\n')
 print("Kruzy pro den:", lines[0].split(" ")[0])
 
@@ -12,12 +12,25 @@ for line in lines:
         line_euro = line
         break
 
-rate_str = line_euro.split('|')[-1]
-rate = float(rate_str.replace(',' , '.'))
+rate_str = line_euro.split('|')[-1].replace(',' , '.')
+rate = float(rate_str)
 
 print("Kurz eura je", rate)
+druh_prevodu = input("Chces prevadet z eur na koruny (1) nebo z korun na eura (2)?")
+if druh_prevodu == "1":
+    try:
+        value_in = float(input("Kolik mas eur? ").replace(',', '.'))
+        value_out = value_in * rate
+        print(f"Tak to je {value_out:.2f} korun.")
+    except ValueError:
+        print("Mel jsi napsat cislo")
 
-value_in = float(input("Kolik mas eur?"))
-value_out = value_in * rate
-
-print(f"Tak to je {value_out:.2f} korun.")
+elif druh_prevodu == "2":
+    try:
+        value_in = float(input("Kolik mas korun? "))
+        value_out = value_in / rate
+        print(f"Tak to je {value_out:.2f} eur.")
+    except ValueError:
+        print("Mel jsi napsat cislo")
+else:
+    print("mel jsi napsat 1 nebo 2!")
